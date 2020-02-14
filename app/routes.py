5464a488573a5a1d396f100c2292e3926373870b
jakob <j.c.hild@email.msmary.edu>
2020-02-14 11:04:49 -0500

    up to date

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
+from flask import render_template
 from app import app
+
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello World!"
-
-
-
+    user = {'username': 'Praise Lord Honk, the bringer of Chaos and Death'}
+    return render_template('index.html', title='Home', user=user)
